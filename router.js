ed3c6321b785599975ce5878059a4d7ac64c4cc0
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-12-21 11:53:27 -0600

    Haciendo match entre la URL y una ruta

--- router.js
+++ router.js
@@ -1,14 +1,33 @@
+const { url } = require("inspector");
+
 class Router {
 
-    constructor(routes){
+    constructor(routes) {
         this.routes = routes;
         this._loadInitialRoute();
     }
-}
 
-_loadInitialRoute() {
-    const pathNameSplit = window.location.pathname.split('/');
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1): '';
+    _matchUrlToRoute(urlSegs) {
+        const matchedRoute = this.routes.find(route => {
+            
+            const routePathSegs = route.path.split('/').slice(1);//Sacamos la informacion que esta despues del /
 
-    this.loadRoute(...pathSegs)
+            if (routePathSegs.length !== url.length) {
+                return false;
+            }
+
+            return routePathSegs
+                .every((routePathSegs, i) => routePathSegs === urlSegs[i])//.every Determina si todos los elementos en el array satisfacen una condición.
+        });
+
+        return matchedRoute;
+    }
+
+    _loadInitialRoute() {
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+
+        this.loadRoute(...pathSegs);
+    }
+
 }
